3747652f3a1493037ec0c5b87a2f5b084c4f3906
machego <114348981+machego@users.noreply.github.com>
2023-02-25 15:24:17 +0100

    first version

---
+++ Character.js
@@ -0,0 +1,56 @@
+import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
+
+
+class Character {
+    constructor(data) {
+        Object.assign(this, data)
+        this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+        this.maxHealth = this.health
+    }
+   
+    setDiceHtml = diceCount => {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceHtml = this.currentDiceScore.map((num) =>
+            `<div class="dice">${num}</div>`).join('')
+    }
+
+    takeDamage = attackScoreArray => {
+        const totalAttackScore = attackScoreArray.reduce((acc, cur) => acc + cur, 0)
+        this.health -= totalAttackScore
+
+        if (this.health <= 0) {
+            this.health = 0
+            this.dead = true
+        }
+    } 
+
+    getHealthBarHtml = () => {
+        const percent = getPercentage(this.health, this.maxHealth)
+        return `
+        <div class="health-bar-outer">
+            <div class="health-bar-inner ${percent <26 ? "danger" : "" } " 
+            style="width: ${percent}%;">
+            </div>
+        </div>`
+    }
+    
+    getCharacterHtml = data => {
+        const { id, name, avatar, health, diceCount, diceHtml } = this
+        const healthBar = this.getHealthBarHtml()
+        return `
+        <div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            ${healthBar}
+            <div class="dice-container">
+                ${diceHtml}
+            </div>
+        </div>`
+    }  
+}
+
+
+export default Character 
+
+
---
+++ utils.js
@@ -0,0 +1,18 @@
+function getDiceRollArray(diceCount) {
+    return new Array(diceCount).fill(0).map(() => Math.floor(Math.random()*6)+1)
+}
+
+
+function getDicePlaceholderHtml(diceCount){
+    return new Array(diceCount).fill(0).map(() => 
+        `<div class="placeholder-dice"></div>`
+    ).join('')
+}
+
+const getPercentage = (remainingHealth, maximumHealth) => 
+    (100 * remainingHealth) / maximumHealth
+
+
+export { getDiceRollArray, getDicePlaceholderHtml, getPercentage }
+
+
